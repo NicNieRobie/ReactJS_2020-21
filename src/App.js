557f2c8fa89aa2275270465dece04c8d7c10cf5d
NicNieRobie <70844046+NicNieRobie@users.noreply.github.com>
2020-10-16 01:14:56 +0300

    HW3 added.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  state = {
+    tasks: [
+    {
+      id: 1,
+      name: 'Task 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum semper vel mi eget varius.',
+      completed: true
+    },
+    {
+      id: 2,
+      name: 'Task 2',
+      description: 'Pellentesque porttitor quam in tincidunt aliquet.',
+      completed: false
+    },
+    {
+      id: 3,
+      name: 'Task 3',
+      description: 'Etiam dapibus rutrum lectus, eu rhoncus ex lacinia sit amet.',
+      completed: true
+    }
+    ]
+  }
+
+  render() {
+    return (
+      this.state.tasks.map((task) => (
+      <div class = "task">
+        <h2 class = "task-header">{task.name}</h2>
+        <div class = "description">
+          <p>{task.description}</p>
+        </div>
+        <span class="status">Completed: {task.completed.toString()}</span>
+        <div class = "completion-button">
+          <button onClick={() => console.log("Task: " + task.name + " completed status = " + task.completed.toString())}>Change completion status</button>
+        </div>
+      </div>
+      ))
+    );
+  }
 }
 
 export default App;
